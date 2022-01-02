505e776978bef8e484b9d20e52e478ff82e6f10e
joabesimao <simaojoabecosta@gmail.com>
2022-01-02 19:46:55 -0300

    adicionar middle roteador

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const app = express();
 const bodyParser = require("body-parser");
 const config = require("config");
 const console = require("console");
+const roteador = require("./api/banco-de-dados/rotas/fornecedores");
 
 app.use(bodyParser.json());
+
+//const roteador = require("./api/banco-de-dados/rotas/fornecedores");
+app.use("/api/fornecedores", roteador);
 
 const PORTA = config.get("api.porta");
 
 app.listen(PORTA, () => console.log(`A API está rodando na porta ${PORTA}`));
-app.listen(config.get("api.porta"), () =>
-  console.log("A API ESTÁ FUNCIONANDO!")
-);
+//app.listen(config.get("api.porta"), () =>
+  //console.log("A API ESTÁ FUNCIONANDO!")
+//);
