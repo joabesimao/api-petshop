b5a1664722533392d436a7ce4c264359431897a8
joabesimao <simaojoabecosta@gmail.com>
2022-01-02 19:12:41 -0300

    apagar o console.log do config.json

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const config = require("config");
+const console = require("console");
 
 app.use(bodyParser.json());
-
-console.log(config.get("api.porta"));
 
 app.listen(config.get("api.porta"), () =>
   console.log("A API ESTÁ FUNCIONANDO!")
