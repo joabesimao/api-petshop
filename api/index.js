8c0d8bdbe1a181f44a2106717ebcab3539fbb9a8
joabesimao <simaojoabecosta@gmail.com>
2022-01-02 17:59:01 -0300

    adicionar arquivo de banco de dados e bibliotecas

--- api/index.js
+++ api/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 app.listen(3000, () => console.log("A API ESTÁ FUNCIONANDO!"));
