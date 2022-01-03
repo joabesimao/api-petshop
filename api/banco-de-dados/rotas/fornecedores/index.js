530b4c05e7cb3838ec9b9d213bfce475176191ff
joabesimao <simaojoabecosta@gmail.com>
2022-01-03 20:25:09 -0300

    criar a rota post

--- api/banco-de-dados/rotas/fornecedores/index.js
+++ api/banco-de-dados/rotas/fornecedores/index.js
@@ -1,8 +1,16 @@
 const roteador = require("express").Router();
 const TabelaFornecedor = require("./TabelaFornecedor");
+const Fornecedor = require("./Fornecedor");
 
-roteador.use("/", async (requisicao, resposta) => {
+roteador.get("/", async (requisicao, resposta) => {
   const resultados = await TabelaFornecedor.listar();
   resposta.send(JSON.stringify(resultados));
+});
+
+roteador.post("/", async (requisicao, resposta) => {
+  const dadosRecebidos = requisicao.body;
+  const fornecedor = new Fornecedor(dadosRecebidos);
+  await fornecedor.criar();
+  resposta.send(JSON.stringify(fornecedor));
 });
 module.exports = roteador;
