2fd122bd100bb28d337f843b6a8fd3a58d1290ae
joabesimao <simaojoabecosta@gmail.com>
2022-01-03 19:39:47 -0300

    cria a primeira rota GET

--- api/banco-de-dados/rotas/fornecedores/index.js
+++ api/banco-de-dados/rotas/fornecedores/index.js
@@ -1,6 +1,8 @@
 const roteador = require("express").Router();
+const TabelaFornecedor = require("./TabelaFornecedor");
 
-roteador.use("/", (requisicao, resposta) => {
-  resposta.send("ok");
+roteador.use("/", async (requisicao, resposta) => {
+  const resultados = await TabelaFornecedor.listar();
+  resposta.send(JSON.stringify(resultados));
 });
 module.exports = roteador;
