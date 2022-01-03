ef2d620ec8025444aafadefd00ba9015b4208c0a
joabesimao <simaojoabecosta@gmail.com>
2022-01-03 19:38:22 -0300

    criar ModeloTabelaFornecedor

--- api/banco-de-dados/rotas/fornecedores/ModeloTabelaFornecedor.js
+++ api/banco-de-dados/rotas/fornecedores/ModeloTabelaFornecedor.js
@@ -13,17 +13,16 @@ const colunas = {
   categoria: {
     type: Sequelize.ENUM("ração", "brinquedos"),
     allowNull: false,
+  },
+};
 
-}
-},
 const opcoes = {
-    freezeTableName:true,
-    tableName : "fornecedores",
-    timestamps: true,
-    createdAt: "dataCriacao",
-    updatedAt:"dataAtualizacao",
-    version: "versao"
-
-}
+  freezeTableName: true,
+  tableName: "fornecedores",
+  timestamps: true,
+  createdAt: "dataCriacao",
+  updatedAt: "dataAtualizacao",
+  version: "versao",
+};
 
 module.exports = instancia.define("fornecedor", colunas, opcoes);
