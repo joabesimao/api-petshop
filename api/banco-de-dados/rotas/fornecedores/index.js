edd5d78afd37c0ac5bc2d6f3654743c4a6749ac6
joabesimao <simaojoabecosta@gmail.com>
2022-01-04 20:27:36 -0300

    criar a rota delete no index

--- api/banco-de-dados/rotas/fornecedores/index.js
+++ api/banco-de-dados/rotas/fornecedores/index.js
@@ -8,10 +8,18 @@ roteador.get("/", async (requisicao, resposta) => {
 });
 
 roteador.post("/", async (requisicao, resposta) => {
-  const dadosRecebidos = requisicao.body;
-  const fornecedor = new Fornecedor(dadosRecebidos);
-  await fornecedor.criar();
-  resposta.send(JSON.stringify(fornecedor));
+  try {
+    const dadosRecebidos = requisicao.body;
+    const fornecedor = new Fornecedor(dadosRecebidos);
+    await fornecedor.criar();
+    resposta.send(JSON.stringify(fornecedor));
+  } catch (erro) {
+    resposta.send(
+      JSON.stringify({
+        mensagem: erro.message,
+      })
+    );
+  }
 });
 
 roteador.get("/:idFornecedor", async (requisicao, resposta) => {
@@ -36,6 +44,21 @@ roteador.put("/:idFornecedor", async (requisicao, resposta) => {
     const dados = Object.assign({}, dadosRecebidos, { id: id });
     const fornecedor = new Fornecedor(dados);
     await fornecedor.atualizar();
+    resposta.end();
+  } catch (erro) {
+    resposta.send(
+      JSON.stringify({
+        mensagem: erro.message,
+      })
+    );
+  }
+});
+roteador.delete("/:idFornecedor", async (requisicao, resposta) => {
+  try {
+    const id = requisicao.params.idFornecedor;
+    const fornecedor = new Fornecedor({ id: id });
+    await fornecedor.carregar();
+    await fornecedor.remover();
     resposta.end();
   } catch (erro) {
     resposta.send(
