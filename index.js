eab0ccb85b38ff11bfbfff75ebae5872004d9a97
joabesimao <simaojoabecosta@gmail.com>
2022-01-04 19:45:51 -0300

    mudando a mensagem da api

--- index.js
+++ index.js
@@ -19,4 +19,4 @@ app.listen(PORTA, () => console.log(`A API está rodando na porta ${PORTA}`));
 
 // to nessa aula aqui =>
 
-//https://cursos.alura.com.br/course/nodejs-api-rest-padronizada-escalavel/task/79831
+//https://cursos.alura.com.br/course/nodejs-api-rest-padronizada-escalavel/task/79832
