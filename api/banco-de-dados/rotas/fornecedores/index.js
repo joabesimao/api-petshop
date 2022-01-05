c1d44c461db90fd7a2c68377a3de74adb996eb35
joabesimao <simaojoabecosta@gmail.com>
2022-01-05 07:29:59 -0300

    adicionar na rota put a classe NaoEncontrado

--- api/banco-de-dados/rotas/fornecedores/index.js
+++ api/banco-de-dados/rotas/fornecedores/index.js
@@ -1,6 +1,7 @@
 const roteador = require("express").Router();
 const TabelaFornecedor = require("./TabelaFornecedor");
 const Fornecedor = require("./Fornecedor");
+const NaoEncontrado = require("../../../erros/NaoEncontrado");
 
 roteador.get("/", async (requisicao, resposta) => {
   const resultados = await TabelaFornecedor.listar();
@@ -52,7 +53,11 @@ roteador.put("/:idFornecedor", async (requisicao, resposta) => {
     resposta.status(204);
     resposta.end();
   } catch (erro) {
-    resposta.status(400);
+    if (erro instanceof NaoEncontrado) {
+      resposta.status(404);
+    } else {
+      resposta.status(400);
+    }
     resposta.send(
       JSON.stringify({
         mensagem: erro.message,
