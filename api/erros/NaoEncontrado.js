c5b24847d5601b5f1a92cb2bc0d9b85802ca3af8
joabesimao <simaojoabecosta@gmail.com>
2022-01-05 07:25:59 -0300

    criar classe de erro NaoEncontrado

---
+++ api/erros/NaoEncontrado.js
@@ -0,0 +1,8 @@
+class NaoEncontrado extends Error {
+  constructor() {
+    super("Fornecedor não foi encontrado !");
+    this.name = "NaoEncontrado";
+    this.idErro = 0;
+  }
+}
+module.exports = NaoEncontrado;
