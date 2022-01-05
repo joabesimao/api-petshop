55bb4765bcde7b7aaa96ce6faf6ee8c0aaea7c6c
joabesimao <simaojoabecosta@gmail.com>
2022-01-05 13:32:01 -0300

    criar meader, para tratar erro NaoEncontrado no index

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ const bodyParser = require("body-parser");
 const config = require("config");
 const console = require("console");
 const roteador = require("./api/banco-de-dados/rotas/fornecedores");
+const NaoEncontrado = require("./api/erros/NaoEncontrado");
 
-app.use(bodyParser.json());
-
-//const roteador = require("./api/banco-de-dados/rotas/fornecedores");
 app.use("/api/fornecedores", roteador);
+
+app.use((erro, requisicao, resposta,proximo) => {
+  if (erro instanceof NaoEncontrado) {
+    resposta.status(404);
+  } else {
+    resposta.status(400);
+  }
+  resposta.send(
+    JSON.stringify({
+      mensagem: erro.message,
+      id: erro.idErro,
+    })
+  );
+});
 
 const PORTA = config.get("api.porta");
 
 app.listen(PORTA, () => console.log(`A API está rodando na porta ${PORTA}`));
-//app.listen(config.get("api.porta"), () =>
-//console.log("A API ESTÁ FUNCIONANDO!")
-//);
-
-// to nessa aula aqui =>
-
-//https://cursos.alura.com.br/course/nodejrs-api-rest-padronizada-escalavel/task/79836
