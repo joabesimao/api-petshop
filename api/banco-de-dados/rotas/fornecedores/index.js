a97360723e4120cad7be1ff572b800fb8096e4d8
joabesimao <simaojoabecosta@gmail.com>
2022-01-04 21:14:18 -0300

    criar resposta.status para erros

--- api/banco-de-dados/rotas/fornecedores/index.js
+++ api/banco-de-dados/rotas/fornecedores/index.js
@@ -4,6 +4,7 @@ const Fornecedor = require("./Fornecedor");
 
 roteador.get("/", async (requisicao, resposta) => {
   const resultados = await TabelaFornecedor.listar();
+  resposta.status(200);
   resposta.send(JSON.stringify(resultados));
 });
 
@@ -12,8 +13,10 @@ roteador.post("/", async (requisicao, resposta) => {
     const dadosRecebidos = requisicao.body;
     const fornecedor = new Fornecedor(dadosRecebidos);
     await fornecedor.criar();
+    resposta.status(201);
     resposta.send(JSON.stringify(fornecedor));
   } catch (erro) {
+    resposta.status(400);
     resposta.send(
       JSON.stringify({
         mensagem: erro.message,
@@ -27,8 +30,10 @@ roteador.get("/:idFornecedor", async (requisicao, resposta) => {
     const id = requisicao.params.idFornecedor;
     const fornecedor = new Fornecedor({ id: id });
     await fornecedor.carregar();
+    resposta.status(200);
     resposta.send(JSON.stringify(fornecedor));
   } catch (erro) {
+    resposta.status(404);
     resposta.send(
       JSON.stringify({
         mensagem: erro.message,
@@ -44,8 +49,10 @@ roteador.put("/:idFornecedor", async (requisicao, resposta) => {
     const dados = Object.assign({}, dadosRecebidos, { id: id });
     const fornecedor = new Fornecedor(dados);
     await fornecedor.atualizar();
+    resposta.status(204);
     resposta.end();
   } catch (erro) {
+    resposta.status(400);
     resposta.send(
       JSON.stringify({
         mensagem: erro.message,
@@ -59,8 +66,10 @@ roteador.delete("/:idFornecedor", async (requisicao, resposta) => {
     const fornecedor = new Fornecedor({ id: id });
     await fornecedor.carregar();
     await fornecedor.remover();
+    resposta.status(204);
     resposta.end();
   } catch (erro) {
+    resposta.status(404);
     resposta.send(
       JSON.stringify({
         mensagem: erro.message,
