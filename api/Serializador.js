434738e3192bfaf897d76501d1d9bb7ecf98547f
joabesimao <simaojoabecosta@gmail.com>
2022-01-06 16:48:30 -0300

    criar classe Serializador

---
+++ api/Serializador.js
@@ -0,0 +1,14 @@
+const ValorNaoSuportado = require("./erros/ValorNaoSuportado");
+
+class Serializador {
+  json(dados) {
+    return JSON.stringify(dados);
+  }
+
+  serializar(dados) {
+    if (this.contentType === "application/json") {
+      return this.json(dados);
+    }
+    throw new ValorNaoSuportado(this.contentType);
+  }
+}
