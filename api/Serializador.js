afe2dfc5b0fe697d260d7d67250f11177ed2a216
joabesimao <simaojoabecosta@gmail.com>
2022-01-09 11:02:27 -0300

    criar o metodo xml

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,13 +1,31 @@
 const ValorNaoSuportado = require("./erros/ValorNaoSuportado");
+const jsontoxml = require("jsontoxml");
 
 class Serializador {
   json(dados) {
     return JSON.stringify(dados);
   }
+  xml(dados) {
+    let tag = this.tagSingular;
+    if (Array.isArray(dados)) {
+      tag = this.tagPlural;
+      dados = dados.map((item) => {
+        return {
+          [this.tagSingular]: item,
+        };
+      });
+    }
+    return jsontoxml({ [tag]: dados });
+  }
 
   serializar(dados) {
+    dados = this.filtrar(dados);
+
     if (this.contentType === "application/json") {
       return this.json(this.filtrar(dados));
+    }
+    if (this.contentType === "application/xml") {
+      return this.xml(dados);
     }
     throw new ValorNaoSuportado(this.contentType);
   }
@@ -22,7 +40,7 @@ class Serializador {
     });
     return novoObjeto;
   }
-  filtrar(dados) { 
+  filtrar(dados) {
     if (Array.isArray(dados)) {
       dados = dados.map((item) => {
         return this.filtrarObjeto(item);
@@ -35,15 +53,30 @@ class Serializador {
 }
 
 class SerializadorFornecedor extends Serializador {
-  constructor(contentType) {
+  constructor(contentType, camposExtras) {
     super();
     this.contentType = contentType;
-    this.camposPublicos = ["id", "empresa", "categoria"];
+    this.camposPublicos = ["id", "empresa", "categoria"].concat(
+      camposExtras || []
+    );
+    this.tagSingular = "fornecedor";
+    this.tagPlural = "fornecedores";
+  }
+}
+
+class SerializadorErro extends Serializador {
+  constructor(contentType, camposExtras) {
+    super();
+    this.contentType = contentType;
+    this.camposPublicos = ["id", "mensagem"].concat(camposExtras || []);
+    this.tagSingular = "erro";
+    this.tagPlural = "erros";
   }
 }
 
 module.exports = {
   Serializador: Serializador,
   SerializadorFornecedor: SerializadorFornecedor,
-  formatosAceitos: ["application/json"],
+  SerializadorErro: SerializadorErro,
+  formatosAceitos: ["application/json", "application/xml"],
 };
