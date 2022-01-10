a16b2c45e0c8a080dd9443ceeb2036b48b341361
joabesimao <simaojoabecosta@gmail.com>
2022-01-10 19:07:16 -0300

    criar modeloTabelaProduto

---
+++ api/banco-de-dados/rotas/fornecedores/produtos/modeloTabelaProduto.js
@@ -0,0 +1,36 @@
+const Sequelize = require("sequelize");
+const instancia = require("../../../../banco-de-dados");
+
+const colunas = {
+  titulo: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  preço: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  estoque: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  fornecedor: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {
+      model: require("../ModeloTabelaFornecedor"),
+      key: "id",
+    },
+  },
+};
+const opcoes = {
+  freezeTableName: true,
+  tableName: "produtos",
+  timestamps: true,
+  createdAt: "dataCriacao",
+  updateAt: "dataAtualizacao",
+  version: "versao",
+};
+
+module.exports = instancia.define("produto", colunas, opcoes);
