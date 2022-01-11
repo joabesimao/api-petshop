533840d9a324e61b983a84c95fd154ddc62aaab4
joabesimao <simaojoabecosta@gmail.com>
2022-01-10 21:25:18 -0300

    criar a funcao remover

--- api/banco-de-dados/rotas/fornecedores/produtos/TabelaProduto.js
+++ api/banco-de-dados/rotas/fornecedores/produtos/TabelaProduto.js
@@ -8,7 +8,17 @@ module.exports = {
       },
     });
   },
+
   inserir(dados) {
     return Modelo.create(dados);
+  },
+
+  remover(idProduto, idFornecedor) {
+    return Modelo.destroy({
+      where: {
+        id: idProduto,
+        fornecedor: idFornecedor,
+      },
+    });
   },
 };
