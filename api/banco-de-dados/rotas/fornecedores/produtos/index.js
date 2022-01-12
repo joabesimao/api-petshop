ae9cd14fe9aba15fe65ef81a52860e6fcc73eca0
joabesimao <simaojoabecosta@gmail.com>
2022-01-11 21:10:06 -0300

    usar o serializador.serializar

--- api/banco-de-dados/rotas/fornecedores/produtos/index.js
+++ api/banco-de-dados/rotas/fornecedores/produtos/index.js
@@ -1,10 +1,12 @@
 const roteador = require("express").Router({ mergeParams: true });
 const Tabela = require("./TabelaProduto");
 const Produto = require("./Produto");
+const Serializador = require("../../../../Serializador").SerializadorProduto;
 
 roteador.get("/", async (requisicao, resposta) => {
-  const produtos = await Tabela.listar(requisicao.fornecedor.idFornecedor);
-  resposta.send(JSON.stringify(produtos));
+  const produtos = await Tabela.listar(requisicao.fornecedor.id);
+  const serializador = new Serializador(resposta.getHeader("Content-Type"));
+  resposta.send(serializador.serializar(produtos));
 });
 
 roteador.post("/", async (requisicao, resposta, proximo) => {
@@ -14,8 +16,9 @@ roteador.post("/", async (requisicao, resposta, proximo) => {
     const dados = Object.assign({}, corpo, { fornecedor: idFornecedor });
     const produto = new Produto(dados);
     await produto.criar();
+    const serializador = new Serializador(resposta.getHeader("Content-Type"));
     resposta.status(201);
-    resposta.send(produto);
+    resposta.send(serializador.serializar(produto));
   } catch (erro) {
     proximo(erro);
   }
@@ -30,5 +33,27 @@ roteador.delete("/:id", async (requisicao, resposta) => {
   await produto.apagar();
   resposta.status(204);
   resposta.end();
+});
+
+roteador.get("/:id", async (requisicao, resposta, proximo) => {
+  try {
+    const dados = {
+      id: requisicao.params.id,
+      fornecedor: requisicao.fornecedor.id,
+    };
+    const produto = new Produto(dados);
+    await produto.carregar();
+    const serializador = new Serializador(resposta.getHeader("Content-Type"), [
+      "preco",
+      "estoque",
+      "fornecedor",
+      "dataCriacao",
+      "dataAtualizacao",
+      "versao",
+    ]);
+    resposta.send(serializador.serializar(produto));
+  } catch (erro) {
+    proximo(erro);
+  }
 });
 module.exports = roteador;
