47ed1f690cf49ceb2c9a546eb35c7ce9fc38defa
joabesimao <simaojoabecosta@gmail.com>
2022-01-13 19:28:35 -0300

    tratar os erros com as classes DadosNaoFornecidos e CampoInvalido

--- api/banco-de-dados/rotas/fornecedores/produtos/Produto.js
+++ api/banco-de-dados/rotas/fornecedores/produtos/Produto.js
@@ -1,4 +1,6 @@
 const Tabela = require("./TabelaProduto");
+const DadosNaoFornecidos = require("../../../../erros/DadosNaoFornecidos");
+const CampoInvalido = require("../../../../erros/CampoInvalido");
 
 class Produto {
   constructor({
@@ -37,10 +39,10 @@ class Produto {
 
   validar() {
     if (typeof this.titulo !== "string" || this.titulo.length === 0) {
-      throw new Error("o campo titulo está invalido");
+      throw new CampoInvalido("titulo");
     }
     if (typeof this.preco !== "number" || this.preco === 0) {
-      throw new Error("o campo preco está invalido");
+      throw new CampoInvalido("preco");
     }
   }
 
@@ -74,7 +76,7 @@ class Produto {
     }
 
     if (Object.keys(dadosParaAtualizar).length === 0) {
-      throw new Error("nao foram fornecidos dados para atualizar");
+      throw new DadosNaoFornecidos();
     }
     return Tabela.atualizar(
       {
