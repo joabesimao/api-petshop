d5bb1b1245eddcbc2c6ef1cb1318838063f4d4a1
joabesimao <simaojoabecosta@gmail.com>
2022-01-14 19:58:13 -0300

    adicionar a rota options

--- api/banco-de-dados/rotas/fornecedores/produtos/index.js
+++ api/banco-de-dados/rotas/fornecedores/produtos/index.js
@@ -3,6 +3,13 @@ const Tabela = require("./TabelaProduto");
 const Produto = require("./Produto");
 const Serializador = require("../../../../Serializador").SerializadorProduto;
 
+roteador.options("/", (requisicao, resposta) => {
+  resposta.set("Access-Control-Allow-Methods", "GET,POST");
+  resposta.set("Access-Control-Allow-Headers", "Content-Type");
+  resposta.status(204);
+  resposta.end();
+});
+
 roteador.get("/", async (requisicao, resposta) => {
   const produtos = await Tabela.listar(requisicao.fornecedor.id);
   const serializador = new Serializador(resposta.getHeader("Content-Type"));
@@ -30,6 +37,13 @@ roteador.post("/", async (requisicao, resposta, proximo) => {
   } catch (erro) {
     proximo(erro);
   }
+});
+
+roteador.options("/:idFornecedor", (requisicao, resposta) => {
+  resposta.set("Access-Control-Allow-Methods", "DELETE,GET,HEAD,PUT");
+  resposta.set("Access-Control-Allow-Headers", "Content-Type");
+  resposta.status(204);
+  resposta.end();
 });
 
 roteador.delete("/:id", async (requisicao, resposta) => {
@@ -110,6 +124,13 @@ roteador.put("/:id", async (requisicao, resposta, proximo) => {
   } catch (erro) {
     proximo(erro);
   }
+});
+
+roteador.options("/:id/diminuir-estoque", (requisicao, resposta) => {
+  resposta.set("Access-Control-Allow-Methods", "POST");
+  resposta.set("Access-Control-Allow-Headers", "Content-Type");
+  resposta.status(204);
+  resposta.end();
 });
 
 roteador.post(
